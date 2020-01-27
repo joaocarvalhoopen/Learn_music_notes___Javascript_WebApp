2f0f42a2a50cb26132e9400dc04c22adc88e5ffe
joaocarvalhoopenlab <joaonunocarv@gmail.com>
2020-01-27 17:33:20 +0000

    Added project link.
    Added project link.

--- learnMusicNotes.js
+++ learnMusicNotes.js
@@ -27,6 +27,9 @@
  * 
  * License
  * MIT Open Source except the SVG images that have their own license. 
+ *
+ * For more details see the project page in GitHub at:
+ *   https://github.com/joaocarvalhoopen/Learn_music_notes___Javascript_WebApp
  * 
  ******************************************************************************
  */
